53833dde02e556fc25e82bd7dae2d6f34122a746
raul8500 <lopezjo299@gmail.com>
2023-05-23 11:09:22 -0600

    code: se agrego la carga de las encuesta de lime survey a la vista de ver grupos

--- CEOA-CS/controllers/LIME_Encuestas.js
+++ CEOA-CS/controllers/LIME_Encuestas.js
@@ -1,26 +1,21 @@
 const request = require('request');
-let sSessionKey = '' // aquí debes usar la clave de sesión que obtuviste previamente
 
-const options = {
+exports.encuestas = async (req, res) => {
+  const sSessionKey = req.cookies.ss;
+  const options = {
     method: 'POST',
     url: 'https://raul.limesurvey.net/admin/remotecontrol',
     headers: {
-        'Content-Type': 'application/json',
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-        method: 'list_surveys',
-        params: [sSessionKey],
-        id: 1,
+      method: 'list_surveys',
+      params: [sSessionKey],
+      id: 1,
     }),
+  };
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+    res.send(body);
+  });
 };
-
-
-exports.encuestas = async (req, res)=>{
-    sSessionKey = req.cookies.ss;
-    console.log(sSessionKey)
-    request(options, function (error, response, body) {
-        if (error) throw new Error(error);
-        console.log(body);
-        res.send(body)
-    });
-}
--- CEOA-CS/public/js/verProyectos.js
+++ CEOA-CS/public/js/verProyectos.js
@@ -1,6 +1,7 @@
 
 //Definición de variables
 const url = 'http://localhost:8081/api/grupos/'
+const urlLIME = 'http://localhost:8081/api/lime/encuestas'
 const contenedor = document.querySelector('tbody')
 const idGrupo = document.getElementById('idGrupo')
 const nombreGrupo = document.getElementById('nombreGrupo')
@@ -12,4 +13,51 @@ function leeselect(){
     const grupo = localStorage.getItem("grupo")
     idGrupo.innerHTML = id
     nombreGrupo.innerHTML = grupo
+}
+
+const formArticulo = document.getElementById('hola')
+const myModalEl = document.getElementById('exampleModal')
+const modal = new mdb.Modal(myModalEl)
+
+
+btnAsignar.addEventListener('click', ()=>{
+    const checkboxes = document.querySelectorAll('.check');
+
+  // Asignar valor a los checkboxes
+   checkboxes.forEach(function(checkbox) {
+    checkbox.checked = false; // true para marcar el checkbox, false para desmarcarlo
+   });
+})
+
+
+//funcion para mostrar los resultados
+
+const mostrar = (encuestas) => {
+    encuestas.result.forEach(encuesta => {
+      resultados += `<tr>
+        <td class="text-center"> <input class="check form-check-input" type="checkbox" value=""/></td>
+        <td class="text-center">${encuesta.sid}</td>
+        <td class="text-center">${encuesta.surveyls_title}</td>
+      </tr>`;
+    });
+    formArticulo.innerHTML = resultados;
+  };
+
+//Procedimiento Mostrar
+fetch(urlLIME)
+    .then( response => response.json() )
+    .then( data => mostrar(data) )
+    .catch( error => console.log(error))
+    const on = (element, event, selector, handler) => {
+    element.addEventListener(event, e => {
+        if(e.target.closest(selector)){
+            handler(e)
+        }
+    })
+} 
+
+function showDetails(e){
+    e.preventDefault();
+    const documento = e.target.parentNode.parentNode.children[1].textContent;
+    console.log(documento)
 }
